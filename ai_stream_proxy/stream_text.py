8231b6052cba81fcc23cdfffe4fbb5ce8af4110c
Jagger <634750802@qq.com>
2025-09-03 03:44:13 +0800

    add a simple stream proxy client

--- ai_stream_proxy/stream_text.py
+++ ai_stream_proxy/stream_text.py
@@ -18,41 +18,61 @@ def stream_text(
     request_messages.append(UIMessage(
         id=str(uuid.uuid4()),
         role='user',
-        parts=[TextUIPart(text=prompt)]
+        parts=[TextUIPart(text=prompt)],
     ))
     response = requests.post(
         f'http://localhost:3000/v1/llm/{provider}/chat',
         json={
             'model': model,
             'messages': list(map(lambda m: m.to_dict(), request_messages)),
-        })
+            **kwargs
+        },
+        stream=True
+    )
 
     if not response.ok:
         raise BaseException(response.text)
 
-    sse_parser = SSEParser()
-    processor = UIMessageStreamProcessor()
+    return StreamTextResponse(response)
 
-    for line in response.iter_lines(delimiter=b'\n\n'):
-        event = sse_parser.parse_sse_line(line.decode('utf-8'))
-        if event.data == '[DONE]':
-            break
-        # print(event.data)
-        # Example:
-        # {"type": "start", "messageId": "2b50779c-5e07-4d00-bd9b-efa49971ae26"}
-        # {"type": "start-step"}
-        # {"type": "text-start", "id": "0"}
-        # {"type": "text-delta", "id": "0", "delta": "Hello! It"}
-        # {"type": "text-delta", "id": "0", "delta": "'s nice to meet"}
-        # {"type": "text-delta", "id": "0", "delta": " you. How"}
-        # {"type": "text-delta", "id": "0", "delta": " are you doing today"}
-        # {"type": "text-delta", "id": "0", "delta": "? Is"}
-        # {"type": "text-delta", "id": "0", "delta": " there anything I can"}
-        # {"type": "text-delta", "id": "0", "delta": " help you with?"}
-        # {"type": "text-end", "id": "0"}
-        # {"type": "finish-step"}
-        # {"type": "finish"}
-        chunk = sse_parser.parse_chunk_json(event.data)
-        processor.process_chunk(chunk)
 
-    return processor.build_message()
+class StreamTextResponse:
+    stream_id: str | None = None
+    response: requests.Response
+
+    def __init__(self, response: requests.Response):
+        self.stream_id = response.headers.get('X-AI-Proxy-Stream-Id')
+        self.response = response
+
+    def read_chunks(self):
+        sse_parser = SSEParser()
+
+        for line in self.response.iter_lines(delimiter=b'\n\n'):
+            if line == b'':
+                continue
+            event = sse_parser.parse_sse_line(line.decode('utf-8'))
+            if event.data == '[DONE]':
+                break
+            # print(event.data)
+            # Example:
+            # {"type": "start", "messageId": "2b50779c-5e07-4d00-bd9b-efa49971ae26"}
+            # {"type": "start-step"}
+            # {"type": "text-start", "id": "0"}
+            # {"type": "text-delta", "id": "0", "delta": "Hello! It"}
+            # {"type": "text-delta", "id": "0", "delta": "'s nice to meet"}
+            # {"type": "text-delta", "id": "0", "delta": " you. How"}
+            # {"type": "text-delta", "id": "0", "delta": " are you doing today"}
+            # {"type": "text-delta", "id": "0", "delta": "? Is"}
+            # {"type": "text-delta", "id": "0", "delta": " there anything I can"}
+            # {"type": "text-delta", "id": "0", "delta": " help you with?"}
+            # {"type": "text-end", "id": "0"}
+            # {"type": "finish-step"}
+            # {"type": "finish"}
+            chunk = sse_parser.parse_chunk_json(event.data)
+            yield chunk
+
+    def read_message(self):
+        processor = UIMessageStreamProcessor()
+        for chunk in self.read_chunks():
+            processor.process_chunk(chunk)
+        return processor.build_message()
--- test_request.py
+++ test_request.py
@@ -1,5 +1,7 @@
 from ai_stream_proxy import stream_text
 
 if __name__ == '__main__':
-    ui_message = stream_text('amazon-bedrock', 'us.anthropic.claude-sonnet-4-20250514-v1:0', [], 'Hello,')
-    print(ui_message.as_text_content())
+    stream_text_response = stream_text('amazon-bedrock', 'us.anthropic.claude-sonnet-4-20250514-v1:0', [], 'Hello,')
+
+    print(stream_text_response.stream_id)
+    print(stream_text_response.read_message().as_text_content())
